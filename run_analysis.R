ffacdcae91bac05f2eb896c1de74ec54fea92498
Smintie007 <linda.bruwer@gmail.com>
2015-02-10 10:37:11 +0200

    Downloading Data

---
+++ run_analysis.R
@@ -0,0 +1,11 @@
+## Downloading Data
+# Set the url for the file to download and download
+localfilepath = "./UCI HAR Dataset.zip"
+
+# Check to see if the zip file is there. If it is then don't download it.
+if (file.exists(localfilepath)){
+        warning("Zip file exists in working directory.")
+} else {
+        fileurl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+        download.file(fileurl, "./UCI HAR Dataset.zip")
+}
