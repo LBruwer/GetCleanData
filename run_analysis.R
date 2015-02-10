b3c54e746b08678ca3b374db5001393bfb516d25
Smintie007 <linda.bruwer@gmail.com>
2015-02-10 10:41:55 +0200

    file time

--- run_analysis.R
+++ run_analysis.R
@@ -9,3 +9,6 @@ if (file.exists(localfilepath)){
         fileurl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
         download.file(fileurl, "./UCI HAR Dataset.zip")
 }
+
+# Print the file time
+file.info(localfilepath)$ctime
